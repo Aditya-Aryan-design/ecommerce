7efc1b814ac29583f4eff946d8d5980d73403992
Aditya-Aryan-design <113804917+Aditya-Aryan-design@users.noreply.github.com>
2024-01-08 23:10:00 +0530

    resolved db connection error

--- DB/conn.ts
+++ DB/conn.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const DB = process.env.DB
 
 
-if(DB){
 
-    mongoose.connect(DB).then(()=>console.log("db connected")).catch(e=>console.log(e))
-}
+
+mongoose.connect(`${DB}`).then(()=>console.log("db connected")).catch(e=>console.log(e))
+
 
